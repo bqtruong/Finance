b4a43ba02522e843e7c08acb55b9626eb44ab1d3
Brian Truong <bqtruong@cox.net>
2016-07-29 01:01:57 -0400

    Basic body scraping

--- CustomFinance.js
+++ CustomFinance.js
@@ -1,88 +1,68 @@
 var request = require("request"),
     cheerio = require("cheerio"),
-    target = "https://www.google.com/finance/company_news?q=NASDAQ%3AYHOO",
-    urls = [],
-    wordBank = [],
-    wordCount = [];
+    target = "https://www.google.com/finance/company_news?q=NASDAQ%3AYHOO";
 
 function scrapeGoogle(page) {
-    var scrape = [];
-    var promise = new Promise(function(resolve, reject) {
+    return new Promise(function(resolve, reject) {
+        var scrape = [];
         request(page, function(error, response, body) //Request to Google Finance News
-        {
-            if (error)
             {
-                reject(Error(request.error));
-            }
-            else
-            {
-                resolve(body);
-            }
+                if (error) {
+                    reject(Error(request.error))
+                } else if (response.statusCode == 200) {
+                    var $ = cheerio.load(body);
+                    var secondURLreg = new RegExp("[^http]http.+(?=&cid)")
+                    $("a", "#news-main").each((i, element) => {
+                        var url = element.attribs.href
+                        if (url.startsWith("http")) {
+                            scrape.push(url.match(secondURLreg)[0].substring(1));
+                        }
+                    });
+                    resolve(scrape);
+                }
+            })
+    });
+};
+
+function scrapeSiteList(links) {
+    var wordBank = []
+    wordCount = [];    
+
+    return new Promise(function(resolve, reject) {
+        links.map((link) => {
+            var docText = "";
+            request(link, function(error, response, body) //Request to links
+                {
+                    if (error) {
+                        reject(Error(request.error));
+                    } else {
+                        var $ = cheerio.load(body);
+                        $("p").each((i, element) => {
+                            if (element.childNodes.length >= 1) {
+                                for (child in element.childNodes) {
+                                    if (element.childNodes[child].nodeType == 3) {
+                                        docText += element.childNodes[child].nodeValue;
+                                    }
+                                }
+                            } else if (element.childNodes.length == 0 && element.nodeType == 3) {
+                                docText += element.nodeValue;
+                            }
+                            docText.replace(/\s+/g, " ").replace(/[^A-Za-z ]/g, "").toLowerCase().split(" ");
+                        });
+                        console.log(docText);
+                        resolve(wordBank, wordCount);
+                    }
+                })
         })
     });
-    promise.then(function(body)
-    {
-        var $ = cheerio.load(body);
-        var secondURLreg = new RegExp("[^http]http.+(?=&cid)")
-        $("a", "#news-main").each(function() 
-        {
-            var url = this.attribs.href;
-            if (url.startsWith("http"))
-            {
-                scrape.push(url.match(secondURLreg)[0].substring(1));
-            }
-        });
-        return scrape;
-    }, function(error)
-    {
-        console.log("Promise rejected: " + error)
-    });
 };
 
-var temp = scrapeGoogle(target);
-console.log(temp);
-
-function scrapeSite(link) {
-    var promise = new Promise(function(resolve, reject) 
-    {
-        request(link, function(error, response, body) //Request to links
-        {
-            if (error)
-            {
-                reject(Error(request.error));
-            }
-            else
-            {
-                resolve(body);
-            }
-            
-        });
-    });
-    promise.then(function(body) 
-    {
-        var $page = cheerio.load(body);
-        var text = $page("body").text().replace(/\s+/g, " ").replace(/[^A-Za-z ]/g, "").toLowerCase().split(" ");
-        text.forEach(function(word)
-        {
-            if (word.length > 15)
-            {
-                text.splice(wordIndex, 1);
-            }
-            else
-            {
-                if (wordBank.indexOf(word) != -1)
-                {
-                    wordCount[wordBank.indexOf(word)]++;
-                }
-                else
-                {
-                    wordBank.push(word);
-                    wordCount.push(1);
-                }
-            }
-        });
-    }, function(error)
-    {
-        console.log("Promise rejected: " + error);
-    });   
-};
+scrapeGoogle(target)
+    .then((list) => {
+        scrapeSiteList(list)
+            .then((bank, count) => {
+                console.log(bank + " " + count);
+            })
+            .catch((error) => console.log("Promise rejected: " + error));
+    })
+    .catch((error) => console.log("Promise rejected: " + error));
